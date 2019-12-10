8c11933060bef49e06a6cb0ec6baae6e84282d37
dandumitriu33 <dandumitriu33@gmail.com>
2019-12-10 10:30:06 +0200

    Add all the pages pre logic

--- app.py
+++ app.py
@@ -4,8 +4,20 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
 def home():
     return render_template('home.html')
+
+
+@app.route('/request-counter', methods=['GET', 'POST'])
+def request_counter():
+
+    return render_template('request-counter.html')
+
+
+@app.route('/statistics')
+def statistics():
+    return render_template('statistics.html')
 
 
 if __name__ == '__main__':
