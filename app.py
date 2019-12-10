b5e398e0f89b7a63f5887138f9801fff48052d6b
dandumitriu33 <dandumitriu33@gmail.com>
2019-12-10 10:53:02 +0200

    Add the count GET requests feature

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+GET_COUNTER = 0
+POST_COUNTER = 0
 
 
 @app.route('/')
@@ -11,13 +14,22 @@ def home():
 
 @app.route('/request-counter', methods=['GET', 'POST'])
 def request_counter():
-
+    global GET_COUNTER, POST_COUNTER
+    if request.method == 'GET':
+        GET_COUNTER += 1
+        return render_template('request-counter.html', get_counter=GET_COUNTER)
+    elif request.method == 'POST':
+        POST_COUNTER += 1
+        return render_template('request-counter.html', post_counter=POST_COUNTER)
     return render_template('request-counter.html')
 
 
 @app.route('/statistics')
 def statistics():
-    return render_template('statistics.html')
+    global GET_COUNTER, POST_COUNTER
+    return render_template('statistics.html',
+                           get_counter=GET_COUNTER,
+                           post_counter=POST_COUNTER)
 
 
 if __name__ == '__main__':
