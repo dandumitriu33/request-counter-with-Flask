1ed3bd85fcad2d82c6d4ed473091d7662fb23154
dandumitriu33 <dandumitriu33@gmail.com>
2019-12-10 11:30:59 +0200

    Add the PUT and DELETE counter features

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 
 GET_COUNTER = 0
 POST_COUNTER = 0
+PUT_COUNTER = 0
+DELETE_COUNTER = 0
 
 
 @app.route('/')
@@ -12,24 +14,32 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/request-counter', methods=['GET', 'POST'])
+@app.route('/request-counter', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def request_counter():
-    global GET_COUNTER, POST_COUNTER
+    global GET_COUNTER, POST_COUNTER, PUT_COUNTER, DELETE_COUNTER
     if request.method == 'GET':
         GET_COUNTER += 1
         return render_template('request-counter.html', get_counter=GET_COUNTER)
     elif request.method == 'POST':
         POST_COUNTER += 1
         return render_template('request-counter.html', post_counter=POST_COUNTER)
+    elif request.method == 'PUT':
+        PUT_COUNTER += 1
+        return render_template('request-counter.html', put_counter=PUT_COUNTER)
+    elif request.method == 'DELETE':
+        DELETE_COUNTER += 1
+        return render_template('request-counter.html', delete_counter=DELETE_COUNTER)
     return render_template('request-counter.html')
 
 
 @app.route('/statistics')
 def statistics():
-    global GET_COUNTER, POST_COUNTER
+    global GET_COUNTER, POST_COUNTER, DELETE_COUNTER, PUT_COUNTER
     return render_template('statistics.html',
                            get_counter=GET_COUNTER,
-                           post_counter=POST_COUNTER)
+                           post_counter=POST_COUNTER,
+                           put_counter=PUT_COUNTER,
+                           delete_counter=DELETE_COUNTER)
 
 
 if __name__ == '__main__':
