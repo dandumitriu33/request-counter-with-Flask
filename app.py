8691ae9900bc208f0344ab2f75d7eadf4e2d51b1
dandumitriu33 <dandumitriu33@gmail.com>
2019-12-16 08:37:35 +0200

    Refactor global variable names

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-GET_COUNTER = 0
-POST_COUNTER = 0
-PUT_COUNTER = 0
-DELETE_COUNTER = 0
+get_counter = 0
+post_counter = 0
+put_counter = 0
+delete_counter = 0
 
 
 @app.route('/')
@@ -14,45 +14,54 @@ def home():
     return render_template('home.html')
 
 
+def test(name):
+    if name == 'John':
+        return 'sorted john'
+
+
 @app.route('/request-counter', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def request_counter():
-    global GET_COUNTER, POST_COUNTER, PUT_COUNTER, DELETE_COUNTER
+    global get_counter, post_counter, put_counter, delete_counter
     if request.method == 'GET':
-        GET_COUNTER += 1
-        return render_template('request-counter.html', get_counter=GET_COUNTER)
+        get_counter += 1
+        return render_template('request-counter.html',
+                               get_counter=get_counter)
     elif request.method == 'POST':
-        POST_COUNTER += 1
-        return render_template('request-counter.html', post_counter=POST_COUNTER)
+        post_counter += 1
+        return render_template('request-counter.html',
+                               post_counter=post_counter)
     elif request.method == 'PUT':
-        PUT_COUNTER += 1
-        return render_template('request-counter.html', put_counter=PUT_COUNTER)
+        put_counter += 1
+        return render_template('request-counter.html',
+                               put_counter=put_counter)
     elif request.method == 'DELETE':
-        DELETE_COUNTER += 1
-        return render_template('request-counter.html', delete_counter=DELETE_COUNTER)
+        delete_counter += 1
+        return render_template('request-counter.html',
+                               delete_counter=delete_counter)
     return render_template('request-counter.html')
 
 
 @app.route('/statistics', methods=['GET', 'POST'])
 def statistics():
-    global GET_COUNTER, POST_COUNTER, PUT_COUNTER, DELETE_COUNTER
+    global get_counter, post_counter, put_counter, delete_counter
     if request.method == 'GET':
         return render_template('statistics.html',
-                               get_counter=GET_COUNTER,
-                               post_counter=POST_COUNTER,
-                               put_counter=PUT_COUNTER,
-                               delete_counter=DELETE_COUNTER)
+                               get_counter=get_counter,
+                               post_counter=post_counter,
+                               put_counter=put_counter,
+                               delete_counter=delete_counter)
     elif request.method == 'POST':
         write_to_file()
         return render_template('statistics.html',
-                               get_counter=GET_COUNTER,
-                               post_counter=POST_COUNTER,
-                               put_counter=PUT_COUNTER,
-                               delete_counter=DELETE_COUNTER)
+                               get_counter=get_counter,
+                               post_counter=post_counter,
+                               put_counter=put_counter,
+                               delete_counter=delete_counter)
 
 
 def write_to_file():
-    global GET_COUNTER, POST_COUNTER, PUT_COUNTER, DELETE_COUNTER
-    data_string = 'GET:' + str(GET_COUNTER) + '\n' + 'POST: ' + str(POST_COUNTER) + '\n' + 'DELETE: ' + str(DELETE_COUNTER) + '\n' + 'PUT: ' + str(PUT_COUNTER)
+    global get_counter, post_counter, put_counter, delete_counter
+    data_string = 'GET:' + str(get_counter) + '\n' + 'POST: ' + str(post_counter) + '\n' + 'DELETE: ' + str(delete_counter) + '\n' + 'PUT: ' + str(put_counter)
     with open('request_counts.txt', 'w') as file:
         file.write(data_string)
 
